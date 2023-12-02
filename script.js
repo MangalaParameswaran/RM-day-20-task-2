c0436abe4d792b0a41dddd63316ad9a4d516b3d7
MangalaParameswaran <mangala214128pa@gmail.com>
2023-12-02 11:40:12 +0530

    style updated

--- script.js
+++ script.js
@@ -17,17 +17,17 @@ fetch("https://restcountries.com/v3.1/all")
       const cardDiv = document.createElement("div");
       cardDiv.classList.add("card1");
       cardDiv.innerHTML = `
-        <div class="card">
+      <div class="card h-auto w-75 mt-4 mx-2 bg-primary">
         <div class="some-class"></div>
-           <h1 id="title" class="text-center">${ele[i].name.common}</h1>
-           <img src="${ele[i].flags.png}" class="card-img-top" alt="country-flag">
+        <div class="bg-secondary-subtle h-75 m-2 rounded-3">
+        <h1 id="title" class="text-center fw-bolder fs-4 pt-2">${ele[i].name.common}</h1>
+        </div>
+        <img src="${ele[i].flags.png}" class="card-img-top h-100 w-75 mt-3 mx-5 rounded-4" alt="country-flag">
           <div class="card-body">
-            <div class="card-text"><b>Capital: ${ele[i].capital}</b></div>
-            <div class="card-text"><b>Region: ${ele[i].region}</b></div>
-            <div class="card-text"><b>Country Code: ${ele[i].latlng}</b></div>
-            <div class="card-text"><b>Country Code: ${ele[i].cca2}</b></div>
-            <button class="btn btn-primary" onclick="getWeatherData('${ele[i].name.common}', ${i})">Click Here for weather</button>
-            <p class="weatherInfo-${i}"></p>
+            <div class="card-text text-center m-3"><b>Capital:<i class="text-light"> ${ele[i].capital}</i></b></div>
+            <div class="card-text text-center m-3"><b>Region:<i class="text-light"> ${ele[i].region}</i> </b></div>
+            <div class="card-text text-center m-3"><b>Country Code:<i class="text-light"> ${ele[i].latlng}</i></b></div>
+            <div class="card-text text-center m-3"><b>Country Code:<i class="text-light"> ${ele[i].cca2}</i></b></div>
             <div class="some-class"></div>
           </div>    
         </div>
@@ -36,26 +36,3 @@ fetch("https://restcountries.com/v3.1/all")
     }
   });
 
-function getWeatherData(restCountryName, index) {
-  let apiKey = "2e058a5c986acfe99cac939f7633466c";
-  let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${restCountryName}&appid=${apiKey}`;
-
-  fetch(weatherUrl)
-    .then((response) => response.json())
-    .then((weatherData) => {
-      var weatherCountryName = weatherData.name;
-      const weatherInformation = document.querySelector(`.weatherInfo-${index}`);
-      
-      if (weatherCountryName === restCountryName) {
-        weatherInformation.innerHTML = `
-          Weather in ${weatherData.name}, ${weatherData.sys.country}: ${weatherData.main.temp_min} min°C and ${weatherData.main.temp_max} max°C
-        `;
-      } else {
-        weatherInformation.innerHTML = `Country names do not match.`;
-      }
-    })
-    .catch((error) => {
-      console.error("Error fetching weather data:", error);
-      alert("Error fetching weather data.");
-    });
-}
